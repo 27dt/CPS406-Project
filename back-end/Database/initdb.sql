b6dddd4de0e086cea054380dd24739b8ef4d091d
Daniel Trakas <danielgtrakas@gmail.com>
2025-03-26 02:11:02 -0400

    db schema changes now include games and ratings

--- back-end/Database/initdb.sql
+++ back-end/Database/initdb.sql
@@ -1,5 +1,26 @@
 CREATE TABLE users(
-    uid BIGINT PRIMARY KEY,
+    uid SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE games(
+    appid INT PRIMARY KEY,
+    name VARCHAR(255),
+    rdate VARCHAR(255),
+    about VARCHAR(2000),
+    img VARCHAR(255),
+    web VARCHAR(255),
+    dev VARCHAR(255),
+    pub VARCHAR(255),
+    genre VARCHAR(100)
+);
+
+CREATE TABLE ratings(
+    userid INT,
+    gameid INT,
+    rating INT NOT NULL,
+    PRIMARY KEY(userid, gameid),
+    FOREIGN KEY (userid) REFERENCES users(uid) ON DELETE CASCADE,
+    FOREIGN KEY (gameid) REFERENCES games(appid) ON DELETE CASCADE
 );
