520bd07168f38b49fe0b19f6711bb33bf5797cac
JoanthanRomao <jonathanromao2005@gmail.com>
2025-03-27 15:31:37 -0400

    added icons and transfering files .

--- front-end/cps406-project/src/pages/DashboardPage.jsx
+++ front-end/cps406-project/src/pages/DashboardPage.jsx
@@ -1,12 +1,11 @@
 import Card from "../components/Card.jsx"
+import Nav from "../components/Nav.jsx"
 
 function DashboardPage() {
 
   return (
     <>
-      <h3>this is the DashboardPage</h3>
-      <Card i='1' numGames={12}/>
-      <Card i='2' numGames={3}/>
+      <Nav />
     </>
   )
 
