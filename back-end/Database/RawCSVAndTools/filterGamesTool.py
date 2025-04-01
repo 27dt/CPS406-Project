2bb840698aed1f681b2773dd6b799b57cca863cd
gogilbert <jsboersema@hotmail.com>
2025-03-31 20:00:19 -0400

    Made docker changes that allow for populating the games DB table from a CSV file

---
+++ back-end/Database/RawCSVAndTools/filterGamesTool.py
@@ -0,0 +1,25 @@
+# Tool used to filter out games that don't meet requirements
+import csv
+
+readFile = "steam_games.csv"
+writeFile = "filter_steam_games.csv"
+
+fields = []
+rows = []
+
+with open(readFile, "r") as csvfile:
+    csvreader = csv.reader(csvfile)
+
+    fields = next(csvreader)
+
+    for row in csvreader:
+        
+        if len(row[1]) <= 254 and len(row[6]) <= 254 and len(row[7]) <= 254 and len(row[8]) <= 254:
+            rows.append(row)
+
+with open(writeFile, "w") as csvFile:
+    csvwriter = csv.writer(csvFile)
+
+    csvwriter.writerow(fields)
+    
+    csvwriter.writerows(rows)
