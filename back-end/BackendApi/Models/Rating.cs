d19f17db1c36bd006e3f2a7d90e43c2feecd6d2e
gogilbert <jsboersema@hotmail.com>
2025-04-04 13:40:26 -0400

    Removed column attributes that didn't do anything

--- back-end/BackendApi/Models/Rating.cs
+++ back-end/BackendApi/Models/Rating.cs
@@ -8,9 +8,7 @@ namespace BackendApi.Models {
     [PrimaryKey(nameof(userid), nameof(gameid))]
     public class Rating {
         // Matches database schema of ratings table ((userid, gameid) composite key).
-        [Column(Order = 1)]
         public required int userid { get; set; }
-        [Column(Order = 2)]
         public required int gameid { get; set; }
         public required int rating { get; set; }
     }
