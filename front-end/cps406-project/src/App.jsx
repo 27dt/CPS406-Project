e062935d7e363794640c456c876125a3b7c475a3
ObsidioSteel <shaheedheadley@hotmail.com>
2025-04-07 23:33:14 -0400

    Merging leftover progress from local desktop repository.

--- front-end/cps406-project/src/App.jsx
+++ front-end/cps406-project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage.jsx'
 import RegisterPage from './pages/RegisterPage.jsx'
+import ForgotPassPage from './pages/ForgotPassPage.jsx'
 import DashboardPage from './pages/DashboardPage.jsx'
 import SearchPage from './pages/SearchPage.jsx'
 import LandingPage from './pages/LandingPage.jsx'
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<DashboardPage />}/>
         <Route path='/login' element={<LoginPage />}/>
         <Route path='/register' element={<RegisterPage />}/>
+        <Route path='/forgotpassword' element={<ForgotPassPage />}/>
         <Route path='/search' element={<SearchPage />}/>
         <Route path='/landingpage' element={<LandingPage />}/>
         <Route path='/gamepage' element={<GamePage />}/>
--- front-end/cps406-project/src/pages/LoginPage.jsx
+++ front-end/cps406-project/src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 import EntryField from "../components/EntryField.jsx"
 import LinkMessage from "../components/LinkMessage.jsx"
@@ -9,21 +9,48 @@ import "./LoginPage.css"
 function LoginPage() {
   const initialValues = {username:"", password:""};
   const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
   const handleChange = (e) => {
     const {name, value} = e.target;
     setFormValues({...formValues, [name]:value});
-  }
+  };
 
   const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
 
-  }
+  useEffect (() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors])
+
+  const validate = (values) => {
+    const errors = {};
+
+    if (!values.username) {
+      errors.username = "Username is required!";
+    }
+    if (!values.password) {
+      errors.password = "Password is required!";
+    } else if (values.password.length < 5) {
+      errors.password = "Password must be at least 6 characters!"
+    } else if (values.password.length > 200) {
+      errors.password = "Password must be less than 200 cha... wait why did you-"
+    }
+    return errors;
+  };
 
   return (
     <>
       <h1>Login</h1>
       <section className="main-content login">
-        <form onSubmit={handleSubmit}>\
+        <form onSubmit={handleSubmit}>
           <EntryField 
             type="text"
             text="Username" 
@@ -32,6 +59,7 @@ function LoginPage() {
             value={formValues.username}
             onChange={handleChange}>
           </EntryField>
+          <p>{formErrors.username}</p>
           <EntryField 
             type="password"
             text="Password" 
@@ -40,11 +68,17 @@ function LoginPage() {
             value={formValues.password}
             onChange={handleChange}>
           </EntryField>
+          <p>{formErrors.password}</p>
           <FormButton text="Login"></FormButton>
         </form>
       </section>
       <section className="link-content login">
-        <a href="/#/login">Forgot your password?<br></br></a>
+      <LinkMessage 
+          beforeText="Forgot your password?" 
+          afterText=" to reset it!" 
+          pagePath="/#/forgotpassword" 
+          linkName="Click here">
+        </LinkMessage>
         <LinkMessage 
           beforeText="Don't have an account?" 
           afterText="instead!" 
